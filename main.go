e41d081b0d3e80e241fea7cabd12a6ffb144ec7a
Hamza <hamzaredone6@gmail.com>
2024-11-17 21:17:25 +0000

    dd

--- main.go
+++ main.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/zaluty/buildbeforecommmit/helpers"
 )
 
 func main() {
 	file := helpers.MustCompile(`^.*\d.txt$`)
-	fmt.Println(file, "test.txt")
 }
